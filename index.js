d37e8f16530e6e27a8374f4466951afea91ae505
Donal Linehan <Donal@Donals-MacBook-Pro.local>
2014-02-21 18:24:24 +0000

    Added linux support - maybe

--- index.js
+++ index.js
@@ -17,11 +17,43 @@ function tick(callback) {
 		});
 	}
 	else if (/linux/.test(process.platform)) {
-		callback(0);
+		getLinuxIdleTime(callback);
 	}
 	else {
 		callback(0);
 	}
+}
+
+
+function getLinuxIdleTime (callback) {
+	var string = 'USER IDLE\ndonal 13.75s\ndonal 27:35\ndonal 19:43\ndonal 7.00s';
+	exec('w | awk "{if (NR!=1) {print $1,$5 }}"\'', function (error, stdout, stderr) {
+		if(error) {
+			throw stderr;
+		}
+		var times = stdout.match(/(\d{1,2}:\d\d)|(\d{1,2}.\d\ds)/g);
+
+		var shortestTime = -1;
+
+		times.forEach(function(string, i){
+			var seconds, tmpArray;
+
+			if(string.indexOf('s') !== -1) {
+				tmpArray = string.split(':');
+				seconds = parseInt(tmpArray[0], 10);
+			}
+			else {
+				tmpArray = string.split(':');
+				seconds = (parseInt(tmpArray[0], 10) * 60) + parseInt(tmpArray[1], 10);
+			}
+
+			if(shortestTime === -1 || seconds < shortestTime) {
+				shortestTime = seconds;
+			}
+		});
+
+		callback(shortestTime);
+	});
 }
 
 exports.tick = tick
