0683a62acaf64190a04910c84fe85a3d0d1d2a25
Donal Linehan <donalin@gmail.com>
2014-02-21 15:11:44 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var win_idle = require('./lib/win-idle');
+var exec = require('child_process').exec;
+
+function tick(callback) {
+	var ms;
+	callback = callback || function (){};
+	
+	if (/^win/.test(process.platform)) {
+		callback(win_idle.idle());
+	}
+	else if (/darwin/.test(process.platform)) {
+		exec('/usr/sbin/ioreg -c IOHIDSystem | /usr/bin/awk \'/HIDIdleTime/ {print int($NF/1000000000); exit}\'', function (error, stdout, stderr) {
+			if(error) {
+				throw stderr;
+			}
+			callback(parseInt(stdout, 10));
+		});
+	}
+	else if (/linux/.test(process.platform)) {
+		callback(0);
+	}
+	else {
+		callback(0);
+	}
+}
+
+
+exports.tick = tick
