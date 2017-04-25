01c47b1b786e2883bd1af561615a4ff2bb3bfb34
Oscar Brito <aetheon@gmail.com>
2017-04-25 08:37:02 +0100

    convert tabs to spaces

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var os = require('os');
 var path = require('path');
 
 var listeners = [],
-	idle = {};
+    idle = {};
 
 // timeout value to ask the system it's idle value
 var AFK_SYSTEM_POLLING_TIMEOUT_MSEC = 2 * 1000;
@@ -13,41 +13,41 @@ var AFK_SYSTEM_POLLING_TIMEOUT_MSEC = 2 * 1000;
 var INTERVAL_AWAY_IDLE_TIME_PERCENTAGE = 0.70;
 
 idle.tick = function (callback) {
-	callback = callback || function (){};
+    callback = callback || function (){};
 
-	if (/^win/.test(process.platform)) {
-		var cmd = path.join( __dirname, 'bin', 'idle.exe');
-		execFile(cmd, function (error, stdout, stderr) {
-			if(error) {
-				callback(0, error);
-				return;
-			}
-			callback(Math.floor(parseInt(stdout, 10) / 1000), null);
-		});
-	}
-	else if (/darwin/.test(process.platform)) {
-		var cmd = '/usr/sbin/ioreg -c IOHIDSystem | /usr/bin/awk \'/HIDIdleTime/ {print int($NF/1000000000); exit}\'';
-		exec(cmd, function (error, stdout, stderr) {
-			if(error) {
-				callback(0, error);
-				return;
-			}
-			callback(parseInt(stdout, 10), null);
-		});
-	}
-	else if (/linux/.test(process.platform)) {
-		var cmd = 'xprintidle';
-		exec(cmd, function (error, stdout, stderr) {
-			if(error) {
-				callback(0, error);
-				return;
-			}
-			callback(Math.round(parseInt(stdout, 10) / 1000), null);
-		});
-	}
-	else {
-		callback(0);
-	}
+    if (/^win/.test(process.platform)) {
+        var cmd = path.join( __dirname, 'bin', 'idle.exe');
+        execFile(cmd, function (error, stdout, stderr) {
+            if(error) {
+                callback(0, error);
+                return;
+            }
+            callback(Math.floor(parseInt(stdout, 10) / 1000), null);
+        });
+    }
+    else if (/darwin/.test(process.platform)) {
+        var cmd = '/usr/sbin/ioreg -c IOHIDSystem | /usr/bin/awk \'/HIDIdleTime/ {print int($NF/1000000000); exit}\'';
+        exec(cmd, function (error, stdout, stderr) {
+            if(error) {
+                callback(0, error);
+                return;
+            }
+            callback(parseInt(stdout, 10), null);
+        });
+    }
+    else if (/linux/.test(process.platform)) {
+        var cmd = 'xprintidle';
+        exec(cmd, function (error, stdout, stderr) {
+            if(error) {
+                callback(0, error);
+                return;
+            }
+            callback(Math.round(parseInt(stdout, 10) / 1000), null);
+        });
+    }
+    else {
+        callback(0);
+    }
 };
 
 idle.addListener = function (intervalSec, callback) {
