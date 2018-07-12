206bcdf96226cee372635b58545a97ba6a7e7677
Alan Smith <alan.smith@teamwork.com>
2018-07-12 19:14:37 +0100

    Rewrite to ES6 + use desktop-idle

--- index.js
+++ index.js
@@ -1,127 +1,43 @@
-var exec = require('child_process').exec;
-var execFile = require('child_process').execFile;
-var os = require('os');
-var path = require('path');
+const Listener = require('./Listener');
 
-var listeners = [],
-    idle = {};
-
-// timeout value to ask the system it's idle value
-var AFK_SYSTEM_POLLING_TIMEOUT_MSEC = 2 * 1000;
-
-// percentage of the interval to be checked against the idle time. This avoid settings dynamic intervals.
-var INTERVAL_AWAY_IDLE_TIME_PERCENTAGE = 0.70;
-
-idle.tick = function (callback) {
-    callback = callback || function (){};
-
-    if (/^win/.test(process.platform)) {
-        var cmd = path.join( __dirname, 'bin', 'idle.exe');
-        execFile(cmd, function (error, stdout, stderr) {
-            if(error) {
-                callback(0, error);
-                return;
-            }
-            callback(Math.floor(parseInt(stdout, 10) / 1000), null);
-        });
+class NodeAFK {
+    constructor() {
+        this.AFK_POLLING_TIMEOUT_MSEC = 2 * 1000;
+        this.INTERVAL_AWAY_IDLE_TIME_PERCENTAGE = 0.7;
+        this.listeners = {};
     }
-    else if (/darwin/.test(process.platform)) {
-        var cmd = '/usr/sbin/ioreg -c IOHIDSystem | /usr/bin/awk \'/HIDIdleTime/ {print int($NF/1000000000); exit}\'';
-        exec(cmd, function (error, stdout, stderr) {
-            if(error) {
-                callback(0, error);
-                return;
-            }
-            callback(parseInt(stdout, 10), null);
-        });
-    }
-    else if (/linux/.test(process.platform)) {
-        var cmd = 'xprintidle';
-        exec(cmd, function (error, stdout, stderr) {
-            if(error) {
-                callback(0, error);
-                return;
-            }
-            callback(Math.round(parseInt(stdout, 10) / 1000), null);
-        });
-    }
-    else {
-        callback(0);
-    }
-};
 
-idle.addListener = function (intervalSec, callback) {
-    var isAfk = false;
+    // Add a listener to check when the user is away
+    // Interval - {Number} of Seconds
+    // callback - {Function}
+    // Returns a {Number} - The id of the listener that was created
+    addListener(interval, callback) {
+        const randomId = Math.ceil(Math.random() * Number.MAX_SAFE_INTEGER);
 
-    var listenerIndex = listeners.push(true) - 1;
-    var defaultIntervalMsec = intervalSec * 1000;
-    var timeoutId = null;
-    var lastCheckDateMsec = null;
-
-    var checkIsAway = function () {
-
-        if(!listeners[listenerIndex]) {
-            clearTimeout(timeoutId);
+        // Just in case of the very very small proability
+        // that the randomId was generated for another listener
+        if (this.listeners[randomId]) {
+            this.addListener(interval, callback);
             return;
         }
 
-        // calculate the duration of the interval. if there's no previous lastCheckDateMsec the duration is a very big number
-        // to have no influence on the afk status.
-        intervalDurationMsec = Number.MAX_VALUE;
-        if(lastCheckDateMsec) {
-            intervalDurationMsec = Date.now() - lastCheckDateMsec;
+        this.listeners[randomId] = new Listener(randomId, interval, callback);
+
+        return randomId;
+    }
+
+    // Remove a listener
+    // id - {Number} id of the listener
+    // Returns a Boolean of the success of the operation
+    removeListener(id) {
+        if (this.listeners[id]) {
+            this.listeners[id].removeListener();
+            delete this.listeners[id];
+            return true
         }
 
-        lastCheckDateMsec = Date.now();
-
-        // ask the system what's the idle time
-        idle.tick(function(idleSeconds, error){
-
-            if(error) {
-                callback({ id: listenerIndex }, error);
-                timeoutId = setTimeout(checkIsAway, defaultIntervalMsec);
-                return;
-            }
-
-            // is aways if the idle duration is bigger than a interval duration fraction
-            isAway = (idleSeconds * 1000) >= (intervalDurationMsec * INTERVAL_AWAY_IDLE_TIME_PERCENTAGE);
-
-            if(!isAfk && isAway) {
-                callback({
-                	status: 'away',
-                	seconds: idleSeconds,
-                	id: listenerIndex
-                });
-
-                isAfk = true;
-            }
-            else if(isAfk && !isAway) {
-                callback({
-                    status: 'back',
-                    seconds: idleSeconds,
-                    id: listenerIndex
-                });
-
-                isAfk = false;
-            }
-
-            timeoutId = setTimeout(checkIsAway, isAfk ? AFK_SYSTEM_POLLING_TIMEOUT_MSEC : defaultIntervalMsec);
-        });
-    };
-
-    checkIsAway();
-
-    return listenerIndex;
-};
-
-idle.removeListener = function (listenerIndex) {
-    if(listenerIndex < listeners.length){
-        listeners.splice(listenerIndex, 1);
-        return true;
-    }
-    else {
         return false;
     }
-};
+}
 
-module.exports = idle;
+module.exports = new NodeAFK();
